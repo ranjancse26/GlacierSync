8236d596b7d77533d8cf2557ebea5e41f1e4202b
Scott Mencer <smencer@gmail.com>
2015-09-11 17:45:41 -0400

    Added new project for service version of the functionality.

---
+++ GlacierSync.Service/BackupService.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace GlacierSync.Service
+{
+	public class BackupService
+	{
+		public BackupService ()
+		{
+		}
+
+		public bool Start(){ return true; }
+		public bool Stop(){ return false; }
+	}
+}
+
---
+++ GlacierSync.Service/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using Topshelf;
+
+namespace GlacierSync.Service
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			HostFactory.Run (configurator => {
+			                 configurator.Service<BackupService>(s =>
+							 {
+								s.ConstructUsing(service => new BackupService());
+								s.WhenStarted(service => service.Start());
+								s.WhenStopped(service => service.Stop());
+							 });
+			                 configurator.RunAsPrompt();
+			                 configurator.SetDescription("Runs a backup/zip/glacier push on a schedule");
+			                 configurator.SetServiceName("");
+			                 configurator.SetDisplayName("");
+			                 configurator.StartAutomatically();
+			                 });
+		}
+	}
+}
